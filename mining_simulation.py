7b31d6dacd92316a287c60b022faa937716a00e8
kckhafra <kckhafra@gmail.com>
2021-10-28 22:32:06 -0400

    might work

--- mining_simulation.py
+++ mining_simulation.py
@@ -55,7 +55,7 @@ def Simulate(alpha,gamma,N, seed):
                 state=0
             else:
                 state=0
-                ChainLength+=1
+                # ChainLength+=1
 
         elif state==2:
             #The selfish pool has 2 hidden block.
@@ -66,7 +66,7 @@ def Simulate(alpha,gamma,N, seed):
                 #The honest miners found a block.
                 state=state-1
                 SelfishRevenue+=2
-                # ChainLength+=1
+                ChainLength+=2
         elif state>2:
             if r<=alpha:
                 #The selfish miners found a new block
